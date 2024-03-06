6c248a1efe5bff12354cf2604d2e4f3535b6073d
Jishu Sardar <73737848+jishusardar@users.noreply.github.com>
2024-03-06 09:08:35 +0530

    Create Maximum_Index_5_Mar_24.cpp

---
+++ Maximum_Index_5_Mar_24.cpp
@@ -0,0 +1,17 @@
+int maxIndexDiff(int a[], int n) 
+    { 
+        int i = 0;
+        int j = n-1;
+        int maximum = INT_MIN;
+        while(i < n){
+            if(a[i] > a[j]){
+                j--;
+            }
+            else{
+                maximum = max(maximum , (j-i));
+                j = n-1;
+                i++;
+            }
+        }
+        return maximum;
+    }
