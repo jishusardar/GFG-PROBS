d8fa54ac08982cd35de95008725206215f7fdafa
Jishu Sardar <73737848+jishusardar@users.noreply.github.com>
2025-04-26 08:00:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/AVL Tree Insertion/avl-tree-insertion.cpp
+++ Difficulty: Hard/AVL Tree Insertion/avl-tree-insertion.cpp
@@ -74,79 +74,68 @@ class Solution {
   public:
     /*You are required to complete this method */
     int getHeight(Node* root)
-{
-    if(!root)
-    return 0;
-    return root->height;
-}
-int getBalance(Node* root)
-{
-    return getHeight(root->left)-getHeight(root->right);
-}
-Node* leftrotate(Node* root)
-{
-    Node* child=root->right;
-    Node* childleft=child->left;
-
-    child->left=root;
-    root->right=childleft;
-    //adjusting heights
-    root->height=1+max(getHeight(root->left),getHeight(root->right));
-    child->height=1+max(getHeight(child->left),getHeight(child->right));
-    return child;
-}
-Node* rightrotate(Node* root)
-{
-    Node* child=root->left;
-    Node* childright=child->right;
-    
-    child->right=root;
-    root->left=childright;
-    //adjusting new heights
-    root->height=1+max(getHeight(root->left),getHeight(root->right));
-    child->height=1+max(getHeight(child->left),getHeight(child->right));
-    return child;
-}
-    Node* insert(Node* root,int val)
     {
         if(!root)
-    return new Node(val);
-    //check the val goes on left
-    if(val<root->data)
-    root->left=insert(root->left,val);
-    //check the val goes on right
-    else if(val>root->data)
-    root->right=insert(root->right,val);
-    //if duplicate return the root;
-    else
-    return root;
-    
-    //initialize height of the nodes
-    root->height=1+max(getHeight(root->left),getHeight(root->right));
-    //check balancing
-    int balance=getBalance(root);
-    //check if it is left left
-    if(balance>1&&val<root->left->data)
-    return rightrotate(root);
-    //check if it is left right
-    else if(balance>1&&val>root->left->data){
-        root->left=leftrotate(root->left);
-        return rightrotate(root);
+        return 0;
+        return root->height;
     }
-    //check if its is right right
-    else if(balance<-1&&val>root->right->data)
-    return leftrotate(root);
-    //check if its is right left 
-    else if(balance<-1&&val<root->right->data){
-        root->right=rightrotate(root->right);
-        return leftrotate(root);
+    int getBalance(Node* root)
+    {
+        return getHeight(root->left)-getHeight(root->right);
     }
-    else
-    return root;
+    Node* leftrotate(Node* root)
+    {
+        Node* child=root->right;
+        Node* childleft=child->left;
+        
+        child->left=root;
+        root->right=childleft;
+        
+        root->height=1+max(getHeight(root->left),getHeight(root->right));
+        child->height=1+max(getHeight(child->left),getHeight(child->right));
+        return child;
+    }
+    Node* rightrotate(Node* root)
+    {
+        Node* child=root->left;
+        Node* childright=child->right;
+        child->right=root;
+        root->left=childright;
+        root->height=1+max(getHeight(root->left),getHeight(root->right));
+        child->height=1+max(getHeight(child->left),getHeight(child->right));
+        return child;
     }
     Node* insertToAVL(Node* node, int data) {
         // Your code here
-        return insert(node,data);
+        if(!node)
+        return new Node(data);
+        
+        if(data<node->data)
+        node->left=insertToAVL(node->left,data);
+        else if(data>node->data)
+        node->right=insertToAVL(node->right,data);
+        else
+        return node;
+        node->height=1+max(getHeight(node->left),getHeight(node->right));
+        int balance=getBalance(node);
+        //L L
+        if(balance>1&&data<node->left->data)
+        return rightrotate(node);
+        // R R
+        if(balance<-1&&data>node->right->data)
+        return leftrotate(node);
+        // L R
+        if(balance>1&&data>node->left->data){
+            node->left=leftrotate(node->left);
+            return rightrotate(node);
+        }
+        //R L
+        if(balance<-1&&data<node->right->data){
+            node->right=rightrotate(node->right);
+            return leftrotate(node);
+        }
+        else
+        return node;
     }
 };
 
