37b0d1754bbf686f1fc75de10470a9122707e5c4
Jishu Sardar <73737848+jishusardar@users.noreply.github.com>
2025-06-09 02:38:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/BST with Dead End/bst-with-dead-end.cpp
+++ Difficulty: Medium/BST with Dead End/bst-with-dead-end.cpp
@@ -1,70 +1,3 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h>
-
-using namespace std;
-
-struct Node
-{
-	int data;
-	struct Node *left;
-	struct Node *right;
-	
-	Node(int x){
-	    data = x;
-	    left = NULL;
-	    right = NULL;
-	}
-};
-
-
-void insert(Node ** tree, int val)
-{
-    Node *temp = NULL;
-    if(!(*tree))
-    {
-        temp = new Node(val);
-        *tree = temp;
-        return;
-    }
-
-    if(val < (*tree)->data)
-    {
-        insert(&(*tree)->left, val);
-    }
-    else if(val > (*tree)->data)
-    {
-        insert(&(*tree)->right, val);
-    }
-
-}
-
-
-
-
-int getCountOfNode(Node *root, int l, int h)
-{
-
-    if (!root) return 0;
-
-
-    if (root->data == h && root->data == l)
-        return 1;
-
-    if (root->data <= h && root->data >= l)
-         return 1 + getCountOfNode(root->left, l, h) +
-                    getCountOfNode(root->right, l, h);
-
-    else if (root->data < l)
-         return getCountOfNode(root->right, l, h);
-
-
-    else return getCountOfNode(root->left, l, h);
-}
-
-
-// } Driver Code Ends
 
 /*The Node structure is
 struct Node {
@@ -75,58 +8,20 @@ Node * right, * left;
 /*You are required to complete below method */
 class Solution{
   public: 
-    void find(Node* root,int start,int end,bool& p)
+    bool find(Node* root,int start,int end)
     {
         if(!root)
-        return;
+        return 0;
         if(!root->left&&!root->right){
-            if(root->data-start==1&&end-root->data==1)
-            p=1;
+          if(root->data-start==1&&end-root->data==1)
+          return 1;
         }
-        find(root->left,start,root->data,p);
-        find(root->right,root->data,end,p);
+        return find(root->left,start,root->data)||find(root->right,root->data,end);
     }
     bool isDeadEnd(Node *root)
     {
-        bool p=0;
-        find(root,0,INT_MAX,p);
-        return p;
+        return find(root,0,INT_MAX);
         //Your code here
     }
 };
 
-
-//{ Driver Code Starts.
-// bool isDeadEnd(Node *root);
-int main()
-{
-    int T;
-    cin>>T;
-    while(T--)
-    {
-        Node *root;
-        Node *tmp;
-    //int i;
-
-        root = NULL;
-
-        int N;
-        cin>>N;
-        for(int i=0;i<N;i++)
-        {
-            int k;
-            cin>>k;
-            insert(&root, k);
-
-        }
-
-     Solution ob;
-     cout<<ob.isDeadEnd(root);
-     cout<<endl;
-    
-cout << "~" << "\n";
-}
-}
-
-
-// } Driver Code Ends
