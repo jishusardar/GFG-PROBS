4009b9b69ea27c8062d6fd3bdd4713928ad5ca9b
Jishu Sardar <jishusardar01@gmail.com>
2025-09-09 01:34:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,17 +1,26 @@
 class Solution {
   public:
-    bool detect(vector<vector<int>>&Adjmat,vector<bool>vis,int v,int parent)
-    {
-        vis[v]=1;
-        for(int i=0;i<Adjmat[v].size();i++){
-            if(Adjmat[v][i]==parent)
-            continue;
-            if(vis[Adjmat[v][i]])
-            return 1;
-            if(detect(Adjmat,vis,Adjmat[v][i],v))
-            return 1;
+    bool detect(vector<vector<int>>& Adjmat, int start, vector<bool>& visited) {
+        queue<pair<int,int>> q; 
+        q.push({start, -1});
+        visited[start] = true;
+
+        while (!q.empty()) {
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+
+            for (int neigh : Adjmat[node]) {
+                if (!visited[neigh]) {
+                    visited[neigh] = true;
+                    q.push({neigh, node});
+                } 
+                else if (neigh != parent) {
+                    return true; // cycle found
+                }
+            }
         }
-        return 0;
+        return false;
     }
     bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
@@ -20,11 +29,13 @@ class Solution {
             Adjmat[edges[i][0]].push_back(edges[i][1]);
             Adjmat[edges[i][1]].push_back(edges[i][0]);
         }
-        vector<bool>vis(V,0);
-        for(int i=0;i<edges.size();i++){
-            if(detect(Adjmat,vis,edges[i][0],edges[i][1]))
-            return 1;
+        vector<bool>visited(Adjmat.size(),0);
+         for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (detect(Adjmat, i, visited))
+                    return true;
+            }
         }
-        return 0;
+        return false;
     }
 };
