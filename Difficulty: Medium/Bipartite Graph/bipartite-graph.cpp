a176d72fdf1ea161d5de88c97091b6960d122e45
Jishu Sardar <jishusardar01@gmail.com>
2025-09-11 01:55:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
@@ -1,5 +1,18 @@
 class Solution {
   public:
+    bool detect(vector<vector<int>>&Adj,int v,int parent,vector<int>&colors)
+    {
+        colors[v]=!colors[parent];
+        for(int i=0;i<Adj[v].size();i++){
+            if(colors[Adj[v][i]]==-1){
+                if(!detect(Adj,Adj[v][i],v,colors))
+                return 0;
+            }
+            if(colors[Adj[v][i]]==colors[v])
+            return 0;
+        }
+        return 1;
+    }
     bool isBipartite(int V, vector<vector<int>> &edges) {
         // Code here
         vector<vector<int>>Adj(V);
@@ -7,20 +20,11 @@ class Solution {
             Adj[edges[i][0]].push_back(edges[i][1]);
             Adj[edges[i][1]].push_back(edges[i][0]);
         }
-        queue<int>q;
-        q.push(0);
         vector<int>colors(V,-1);
         colors[0]=0;
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(int i=0;i<Adj[node].size();i++){
-                if(colors[Adj[node][i]]==-1)
-                {
-                    q.push(Adj[node][i]);
-                    colors[Adj[node][i]]=!colors[node];
-                }
-                if(colors[Adj[node][i]]==colors[node])
+        for(int i=1;i<V;i++){
+            if(colors[i]==-1){
+                if(!detect(Adj,i,0,colors))
                 return 0;
             }
         }
