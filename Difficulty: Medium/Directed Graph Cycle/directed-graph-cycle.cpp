5c1d14fc7add587fd24262932a97fac8c7df9b8f
Jishu Sardar <jishusardar01@gmail.com>
2025-09-10 12:47:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,17 +1,39 @@
 class Solution {
   public:
-     bool detect(vector<vector<int>>&Adjmat,int v,vector<bool>&visited,vector<bool>&paths)
+     bool detect(vector<vector<int>>&Adjmat,int v,vector<bool>&visited)
     {
-        visited[v]=1;
-        paths[v]=1;
-        for(int i=0;i<Adjmat[v].size();i++){
-            if(paths[Adjmat[v][i]])
-            return 1;
-            if(detect(Adjmat,Adjmat[v][i],visited,paths))
-            return 1;
+        vector<int>indeg(v,0);
+        for(int i=0;i<Adjmat.size();i++)
+        for(int j=0;j<Adjmat[i].size();j++)
+        indeg[Adjmat[i][j]]++;
+        queue<int>q;
+        for(int i=0;i<v;i++){
+            if(indeg[i]==0)
+            q.push(i);
         }
-        paths[v]=0;
+        if(q.empty())
+        return 1;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            visited[node]=1;
+            for(int i=0;i<Adjmat[node].size();i++){
+                indeg[Adjmat[node][i]]--;
+                if(indeg[Adjmat[node][i]]==0)
+                q.push(Adjmat[node][i]);
+            }
+        }
+        bool flag=false;
+        for(int i=0;i<visited.size();i++){
+            if(visited[i]==0){
+                flag=true;
+                break;
+            }
+        }
+        if(q.empty()&&flag)
+        return 1;
         return 0;
+        
     }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
@@ -20,11 +42,7 @@ class Solution {
             Adjmat[edges[i][0]].push_back(edges[i][1]);
         }
         vector<bool>visited(V,0);
-        vector<bool>paths(V,0);
-        for(int i=0;i<V;i++){
-          if(!visited[i]&&detect(Adjmat,i,visited,paths))
-          return 1;
-        }
-        return 0;
+        return detect(Adjmat,V,visited);
+        
     }
 };
