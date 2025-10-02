2c7e3eae1bdbdd5a201b40f8c7fef104cf30b1f1
Jishu Sardar <jishusardar01@gmail.com>
2025-10-03 02:25:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -0,0 +1,19 @@
+// User function template for C++
+
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        // Code here
+        for(int k=0;k<dist.size();k++){
+            for(int i=0;i<dist.size();i++){
+                for(int j=0;j<dist.size();j++){
+                    if(dist[i][k]==1e8)
+                    continue;
+                    if(dist[k][j]==1e8)
+                    continue;
+                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+    }
+};
