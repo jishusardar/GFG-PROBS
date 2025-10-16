8690464d87f23bf56554b8e5772d769d70cfe411
Jishu Sardar <jishusardar01@gmail.com>
2025-10-16 14:03:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,30 +1,51 @@
 class Solution {
   public:
+    int findparent(int u,vector<int>&parent)
+    {
+        if(u==parent[u])
+        return u;
+        return parent[u]=findparent(parent[u],parent);
+    }
+    void UnionByRanks(int u,int v,vector<int>&parent,vector<int>&ranks)
+    {
+        int pu=findparent(u,parent);
+        int pv=findparent(v,parent);
+        if(ranks[pu]>ranks[pv]){
+            parent[pv]=pu;
+        }
+        else if(ranks[pv]>ranks[pu]){
+            parent[pu]=pv;
+        }
+        else{
+            parent[pv]=pu;
+            ranks[pv]++;
+        }
+    }
     int spanningTree(int V, vector<vector<int>>& edges) {
         // code here
-        //without parent
-        vector<bool>Mst(V,0);
-        vector<vector<pair<int,int>>>Adj(V);
-        for(int i=0;i<edges.size();i++){
-            Adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
-            Adj[edges[i][1]].push_back({edges[i][0],edges[i][2]});
+        vector<int>parent(V);
+        vector<int>ranks(V,0);
+        vector<pair<int,pair<int,int>>>arr;
+        for(int i=0;i<V;i++){
+            parent[i]=i;
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
-        q.push({0,0});
-        int ans=0;
-        while(!q.empty()){
-            int wt=q.top().first;
-            int node=q.top().second;
-            q.pop();
-            if(Mst[node])
+        for(int i=0;i<edges.size();i++){
+            arr.push_back({edges[i][2],{edges[i][0],edges[i][1]}});
+        }
+        int ans=0,edge=0;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq(arr.begin(),arr.end());
+        while(!pq.empty()){
+            int wt=pq.top().first;
+            int u=pq.top().second.first;
+            int v=pq.top().second.second;
+            pq.pop();
+            if(findparent(u,parent)==findparent(v,parent))
             continue;
-            Mst[node]=1;
             ans+=wt;
-            for(int i=0;i<Adj[node].size();i++){
-                if(!Mst[Adj[node][i].first]){
-                    q.push({Adj[node][i].second,Adj[node][i].first});
-                }
-            }
+            UnionByRanks(u,v,parent,ranks);
+            edge++;
+            if(edge==V-1)
+            break;
         }
         return ans;
     }
