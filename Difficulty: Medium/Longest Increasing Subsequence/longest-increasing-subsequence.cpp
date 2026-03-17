59ce290acf0a8e38d1460d65d17e4d167c1ae262
Jishu Sardar <jishusardar01@gmail.com>
2026-03-17 15:01:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -3,18 +3,27 @@ class Solution {
     int lis(vector<int>& arr) {
         // code here
         int n=arr.size();
-        vector<int>temp(n,1);
-        for(int i=0;i<n;i++){
-            for(int j=i-1;j>=0;j--){
-                if(arr[i]>arr[j]){
-                    temp[i]=max(temp[i],temp[j]+1);
+        int start,end,size=0,index;
+        vector<int>temp(n);
+        temp[0]=arr[0];
+        for(int i=1;i<n;i++){
+            start=0,end=size,index=size+1;
+            while(start<=end){
+                int mid=start+(end-start)/2;
+                if(temp[mid]==arr[i]){
+                    index=mid;
+                    break;
+                }
+                else if(temp[mid]<arr[i])
+                start=mid+1;
+                else{
+                    index=mid;
+                    end=mid-1;
                 }
             }
+            temp[index]=arr[i];
+            size=max(size,index);
         }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans=max(ans,temp[i]);
-        }
-        return ans;
+        return size+1;
     }
 };
