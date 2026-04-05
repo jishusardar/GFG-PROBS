e19202d9fc91d90cd747ec17e65c0a6e7d615478
Jishu Sardar <jishusardar01@gmail.com>
2026-04-05 16:52:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.cpp
+++ Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.cpp
@@ -1,19 +1,17 @@
 class Solution {
   public:
-    int find(int index,int n,vector<int>&arr,int t,vector<vector<int>>&dp){
-        if(index==n)
-        return 0;
-        if(dp[index][t]!=-1)
-        return dp[index][t];
-        if(!t)
-        return dp[index][t]=max(-arr[index]+find(index+1,n,arr,1,dp),find(index+1,n,arr,0,dp));
-        else
-        return dp[index][t]=max(arr[index]+find(index+1,n,arr,0,dp),find(index+1,n,arr,1,dp));
-    }
     int stockBuySell(vector<int> &arr) {
         // code here
         int n=arr.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        return find(0,n,arr,0,dp);
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                if(!j)
+                dp[i][j]=max(-arr[i]+dp[i+1][1],dp[i+1][0]);
+                else
+                dp[i][j]=max(arr[i]+dp[i+1][0],dp[i+1][1]);
+            }
+        }
+        return dp[0][0];
     }
 };
