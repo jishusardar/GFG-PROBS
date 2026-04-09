0aa7e05b6a00d393cdce2e476c8545afc4861344
Jishu Sardar <jishusardar01@gmail.com>
2026-04-09 10:31:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Unique Paths/number-of-unique-paths.cpp
+++ Difficulty: Medium/Number of Unique Paths/number-of-unique-paths.cpp
@@ -5,18 +5,12 @@ class Solution {
     // Function to find total number of unique paths.
     int NumberOfPath(int a, int b) {
         // code here
-        vector<vector<int>>grid(a,vector<int>(b));
-        for(int i=0;i<a;i++){
-            grid[i][b-1]=1;
+        vector<int>dp(b,1);
+    for (int i=a-2;i>=0;i--) {
+        for (int j=b-2;j>=0;j--) {
+            dp[j]+=dp[j+1];
         }
-        for(int i=0;i<b;i++){
-            grid[a-1][i]=1;
-        }
-        for(int i=a-2;i>=0;i--){
-            for(int j=b-2;j>=0;j--){
-                grid[i][j]=grid[i][j+1]+grid[i+1][j];
-            }
-        }
-        return grid[0][0];
+    }
+    return dp[0];
     }
 };
