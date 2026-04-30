60118d0a46c12196892cd5a9110bc551d1ffe68f
Jishu Sardar <jishusardar01@gmail.com>
2026-04-30 16:41:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
@@ -1,20 +1,15 @@
 class Solution {
   public:
-    int find(int sum,int n,vector<int>&coins,vector<vector<int>>&dp) {
-    if (sum==0)
-        return 1;
-    if (n==0)
-        return 0;
-    if (dp[n][sum]!=-1)
-        return dp[n][sum];
-    if (coins[n-1]>sum)
-        return find(sum,n-1,coins,dp);
-    return dp[n][sum]=find(sum-coins[n-1],n,coins,dp)+find(sum,n-1,coins,dp);
-}
     int count(vector<int>& coins, int sum) {
         // code here.
         int n=coins.size();
-    vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-    return find(sum,n,coins,dp);
+    vector<int>dp(sum+1,0);
+        dp[0]=1;
+    for (int i=1;i<=n;i++) {
+        for (int j=coins[i-1];j<=sum;j++) {
+            dp[j]=dp[j-coins[i-1]]+dp[j];
+        }
+    }
+    return dp[sum];
     }
 };
